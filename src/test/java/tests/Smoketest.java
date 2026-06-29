6ee54fd531e34d864b8e61c77ff3256727d35a37
Test User <test@example.com>
2026-06-29 11:29:21 +0530

    first commit

---
+++ src/test/java/tests/Smoketest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Smoketest {
+
+    @Test
+    public void verifyProjectWorks() {
+
+        System.out.println("Project Ready");
+
+    }
+    
+}
